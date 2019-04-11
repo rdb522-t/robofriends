2328a5729ecce3410e3f58c9f82395cc923369ea
Ryan <ryandbuskirk@gmail.com>
2019-04-11 19:51:55 -0400

    Updates

---
+++ precache-manifest.4a81423e01579376e04bef64d9fa1065.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "e76a3aae7d17dcab2865",
+    "url": "/robofriends/static/js/main.e7addbf0.chunk.js"
+  },
+  {
+    "revision": "7d8b1be3320234fd25fb",
+    "url": "/robofriends/static/js/2.81bd3baa.chunk.js"
+  },
+  {
+    "revision": "e76a3aae7d17dcab2865",
+    "url": "/robofriends/static/css/main.c16e4306.chunk.css"
+  },
+  {
+    "revision": "7d8b1be3320234fd25fb",
+    "url": "/robofriends/static/css/2.10076e99.chunk.css"
+  },
+  {
+    "revision": "6515fc94a32d4900d4ecbc3e0940dd94",
+    "url": "/robofriends/index.html"
+  }
+];
